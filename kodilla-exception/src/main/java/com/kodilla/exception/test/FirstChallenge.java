b8a517931c2e5f4c20edbdc1554d4ba4daa2c06d
SzczotkaT <tomasz.szczotka89@gmail.com>
2018-09-19 19:29:32 +0200

    Task 8.2 First challenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,10 +2,6 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-
-        try{
-
-        }
         if(b == 0){
             throw new ArithmeticException();
         }
@@ -20,9 +16,14 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
 
-        System.out.println(result);
-
+        }catch (ArithmeticException e) {
+            System.out.println("You can't do it..." + e);
+        }finally {
+            System.out.println("Good to try...");
+        }
     }
 }
