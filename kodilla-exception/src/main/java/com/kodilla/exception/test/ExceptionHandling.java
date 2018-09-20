56315ce217402e614e8aefc7cd17f1f1391768b7
SzczotkaT <tomasz.szczotka89@gmail.com>
2018-09-20 12:35:15 +0200

    Task 8.3 Exception handling

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args){
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            secondChallenge.probablyIWillThrowException(1.5, 20);
+        }catch (Exception e){
+            System.out.println("Be careful, You can't do it");
+        }finally {
+            System.out.println("Try or catch");
+        }
+
+    }
+}
+
