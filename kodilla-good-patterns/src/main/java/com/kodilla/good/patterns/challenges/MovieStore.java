6e43cd37178dd0399a9fd4664f5ca37ef4c8e615
SzczotkaT <tomasz.szczotka89@gmail.com>
2018-09-21 11:31:54 +0200

    Task 9.1 Stream reminder

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,5 +1,39 @@
 package com.kodilla.good.patterns.challenges;
 
-public class MovieStore {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class MovieStore {
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> filmsTitlesWithTranslations = new HashMap<>();
+        filmsTitlesWithTranslations.put("IM", ironManTranslations);
+        filmsTitlesWithTranslations.put("AV", avengersTranslations);
+        filmsTitlesWithTranslations.put("FL", flashTranslations);
+
+        return filmsTitlesWithTranslations;
+    }
+
+    public static void main(String[] args){
+        MovieStore movieStore = new MovieStore();
+        movieStore.getMovies().entrySet().stream()
+                .flatMap(m -> m.getValue().stream())
+                .map(l -> l + " ! ")
+                .forEach(System.out::print);
+
+}
 }
